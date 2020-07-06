c41bcd672ec700b47f1307b601ee65daded42ce2
hulinfa <1225488947@qq.com>
2020-07-06 16:46:33 +0800

    提交合并小文件的的初始代码

---
+++ src/test/java/com/run/TestMerge.java
@@ -0,0 +1,30 @@
+package com.run;
+
+import com.runone.MergeFile;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+
+public class TestMerge {
+    MergeFile mergeFile;
+
+    @Before
+//    public void setup() {
+//        mergeFile = new MergeFile();
+//    }
+
+
+    @Test
+    public void testList() {
+        mergeFile.run();
+    }
+
+    @Test
+    public void testRandom(){
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            System.out.println(random.nextInt(900)+100);
+        }
+    }
+}
